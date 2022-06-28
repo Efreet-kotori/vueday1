4f750a5103afbefc4d6377cd8af719980811348e
kotoriefreet <1063377232@qq.com>
2022-06-28 19:57:21 +0800

    v-on修饰符

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
- <a @click="one" href="http://www.baidu.com">阻止百度</a>
-    <hr>
-    <a @click="two(10, $event)" href="http://www.baidu.com">阻止去百度</a>
+    <div @click="fatherFn">
+    <!-- vue对事件进行了修饰符设置, 在事件后面.修饰符名即可使用更多的功能 -->
+    <button @click.stop="btn">.stop阻止事件冒泡</button>
+    <a href="http://www.baidu.com" @click.prevent="btn">.prevent阻止默认行为</a>
+    <button @click.once="btn">.once程序运行期间, 只触发一次事件处理函数</button>
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'App',
   data() { // 格式固定, 定义vue数据之处
     return {  // key相当于变量名
      
     }
   },
   methods: { // vue 的方法写在 里边
-            // 1. 事件触发, 无传值, 可以直接获取事件对象
-    one(e){
-      e.preventDefault()
+          fatherFn(){
+      console.log("father被触发");
     },
-    // 2. 事件触发, 传值, 需要手动传入$event
-    two(num, e){
-      e.preventDefault()
+    btn(){
+      console.log(1);
     }
         }
 }
