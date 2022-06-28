bf9b73f3886a82fa737c97dd43b6c59e0d7f6b92
kotoriefreet <1063377232@qq.com>
2022-06-28 20:07:26 +0800

    百度换肤

--- src/App.vue
+++ src/App.vue
@@ -1,62 +1,57 @@
 <template>
-  <div>
-    <div>
-      <span>选择所在地区：</span>
-      <input type="radio" value="中国大陆" name="area" v-model="area">中国大陆
-      <input type="radio" value="中国香港" name="area" v-model="area">中国香港
+  <div class="container">
+    <div class="imglist">
+      <h3>图片列表区</h3>
+      <img src="./assets/01.jpg" alt="" @mouseenter="enterFn(1)" @click="clickFn(1, $event)">
+      <img src="./assets/02.jpg" alt="" @mouseenter="enterFn(2)"
+      @click="clickFn(2, $event)">
+      <img src="./assets/03.jpg" alt="" @mouseenter="enterFn(3)"
+      @click="clickFn(3, $event)">
     </div>
-    <div>
-      <span>手机号码：</span>
-      <input type="text" name="phone" v-model="phone">
+    <div class="show">
+      <h3>图片预览区</h3>
+      <img :src="imgUrl" alt="" style="width:600px;height:auto;">
     </div>
-    <div>
-      <span>年龄: </span>
-      <input type="text" name="age" v-model="age">
-    </div>
-    <div>
-      <span>密码：</span>
-      <input type="password" name="password" v-model="password">
-    </div>
-    <div>
-      <span>爱好：</span>
-      <input type="checkbox" v-model="hobby" value="看书">看书
-      <input type="checkbox" v-model="hobby" value="学习">学习
-      <input type="checkbox" v-model="hobby" value="写代码">写代码
-      <input type="checkbox" v-model="hobby" value="游戏">游戏
-    </div>
-    <div>
-      <input type="checkbox" v-model="isgrow"> 记住密码
-    </div>
-    ================================================================
-    <div>
-      我来自：{{area}}
-      <br>
-      手机号码是：{{phone}}
-      <br>
-      年龄是：{{age}}
-      <br>
-      密码是：{{password}}
-      <br>
-      爱好是：{{hobby.join(',')}}
-      <br>
-      是否记住密码：{{isgrow ? '是' : '否'}}
-    </div>
-
-
+   <div>
+   </div>
   </div>
 </template>
 
 <script>
+import img_01 from '@/assets/01.jpg';
+import img_02 from '@/assets/02.jpg';
+import img_03 from '@/assets/03.jpg';
 export default {
   data() {
     return {  
-      phone: '',
-      password: '',
-      hobby: ['学习', '写代码'],
-      age:'',
-      area: '中国大陆',
-      isgrow: ''
+      imgUrl: img_01
     };
+  },
+  methods: {
+    enterFn(val) {
+     this.imgUrl = {1:img_01,2:img_02,3:img_03}[val]
+    },
+    clickFn(num, e) {
+      alert(`第${num}张图${e.target.src}`);
+    }
   }
-  }
+}
 </script>
+
+<style scoped>
+  .container {
+    display:flex;
+  }
+ .imglist { 
+  border: 2px solid red;
+ }
+.imglist img {
+  width: 200px;
+  height: auto;
+}
+.show {
+  border: 2px solid red;
+  margin-right: 20px;
+}
+
+</style>
