ff994037c28927e684df9e9b03e11595db780fa4
kotoriefreet <1063377232@qq.com>
2022-06-28 20:05:34 +0800

    搜集用户信息

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,62 @@
 <template>
-    <!-- 1 .number修饰符-把值parseFloat转数值再赋予给v-model对应的变量
-    2 .trim修饰 - 去除首尾两边空格
-    3 .lazy修饰符 - 失去焦点内容改变时(onchange事件), 把内容同步给v-model的变量 -->
   <div>
     <div>
-      <span>年龄:</span>
-      <input type="text" v-model.number="age">
+      <span>选择所在地区：</span>
+      <input type="radio" value="中国大陆" name="area" v-model="area">中国大陆
+      <input type="radio" value="中国香港" name="area" v-model="area">中国香港
     </div>
     <div>
-      <span>人生格言:</span>
-      <input type="text" v-model.trim="motto">
+      <span>手机号码：</span>
+      <input type="text" name="phone" v-model="phone">
     </div>
     <div>
-      <span>自我介绍:</span>
-      <textarea v-model.lazy="intro"></textarea>
+      <span>年龄: </span>
+      <input type="text" name="age" v-model="age">
     </div>
+    <div>
+      <span>密码：</span>
+      <input type="password" name="password" v-model="password">
+    </div>
+    <div>
+      <span>爱好：</span>
+      <input type="checkbox" v-model="hobby" value="看书">看书
+      <input type="checkbox" v-model="hobby" value="学习">学习
+      <input type="checkbox" v-model="hobby" value="写代码">写代码
+      <input type="checkbox" v-model="hobby" value="游戏">游戏
+    </div>
+    <div>
+      <input type="checkbox" v-model="isgrow"> 记住密码
+    </div>
+    ================================================================
+    <div>
+      我来自：{{area}}
+      <br>
+      手机号码是：{{phone}}
+      <br>
+      年龄是：{{age}}
+      <br>
+      密码是：{{password}}
+      <br>
+      爱好是：{{hobby.join(',')}}
+      <br>
+      是否记住密码：{{isgrow ? '是' : '否'}}
+    </div>
+
+
   </div>
 </template>
 
 <script>
 export default {
   data() {
-    return {
-      age: "",
-      motto: "",
-      intro: ""
-    }
+    return {  
+      phone: '',
+      password: '',
+      hobby: ['学习', '写代码'],
+      age:'',
+      area: '中国大陆',
+      isgrow: ''
+    };
   }
-}
+  }
 </script>
-
-<style>
-
-</style>
