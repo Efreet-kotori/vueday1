d349acbaa345f60fcf919e6d3fdfa8326194f4d9
kotoriefreet <1063377232@qq.com>
2022-06-28 19:58:42 +0800

    按键修饰符

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
-    <div @click="fatherFn">
-    <!-- vue对事件进行了修饰符设置, 在事件后面.修饰符名即可使用更多的功能 -->
-    <button @click.stop="btn">.stop阻止事件冒泡</button>
-    <a href="http://www.baidu.com" @click.prevent="btn">.prevent阻止默认行为</a>
-    <button @click.once="btn">.once程序运行期间, 只触发一次事件处理函数</button>
+  <div>
+    <!-- 1. 绑定键盘按下事件.enter - 回车 
+         2. .esc修饰符 - 取消键-->
+    <input type="text" @keydown.enter="enterFn">
+    <hr>
+    <input type="text" @keydown.esc="escFn">
   </div>
 </template>
 
@@ -16,12 +17,12 @@ export default {
     }
   },
   methods: { // vue 的方法写在 里边
-          fatherFn(){
-      console.log("father被触发");
-    },
-    btn(){
-      console.log(1);
-    }
+        enterFn(){
+     console.log("enter回车按键了");
+   },
+   escFn(){
+     console.log("esc按键了");
+   }
         }
 }
 </script>
