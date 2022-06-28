8c77858d1b7cc5175d2b195eabd544b7a7ec0980
kotoriefreet <1063377232@qq.com>
2022-06-28 20:39:54 +0800

    找出字母出现的次数

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
 {{fn(arr)}}
+{{sn(str.split(""))}}
   </div>
 </template>
 
@@ -9,7 +10,8 @@
 export default {
   data() {
     return {  
-       arr : [{a:100}, {a: 20}, {a:-10}]
+       arr : [{a:100}, {a: 20}, {a:-10}],
+       str:'asdasdasdasaaasdsadsa'
     };
   },
   methods: {
@@ -18,6 +20,18 @@ const sum = arr.reduce((pre,curr) => {
   return pre+curr.a
 },0)
 console.log(sum) // 110
+   },
+   sn(names ){
+    let countedNames = names.reduce(function (allNames, name) {
+  if (name in allNames) {
+    allNames[name]++
+  }
+  else {
+    allNames[name] = 1
+  }
+  return allNames
+}, {})
+console.log(countedNames);
    }
 }}
 </script>
