8505bef0abc5a462c20694a09c1b4223476b63ff
kotoriefreet <1063377232@qq.com>
2022-06-28 20:03:00 +0800

    v-model

--- src/App.vue
+++ src/App.vue
@@ -1,48 +1,68 @@
 <template>
-  <!-- 思路
-  1. 变量准备-静态页面铺设
-  2. 绑定点击事件 
-  3. 截取字符串返回数组
-  4. 翻转
-  5. 数组拼接起来 -->
-  <div>
-    <!-- 1. 变量准备-静态页面铺设 -->
-    <h1>{{ message }}</h1>
-     <!-- 2. 绑定点击事件 -->
-    <button @click="btn">逆转世界</button>
+  <div >
+    <!-- 
+    	v-model:是实现vuejs 变量和表单标签value属性, 双向绑定的指令
+    -->
+    <div>
+      <span>用户名:</span>
+      <input type="text" v-model="username" />
+    </div>
+    <div>
+      <span>密码:</span>
+      <input type="password" v-model="pass" />
+    </div>
+    <div>
+      <span>来自于: </span>
+      <!-- 下拉菜单要绑定在select上 -->
+      <select v-model="from">
+        <option value="北京市">北京</option>
+        <option value="南京市">南京</option>
+        <option value="天津市">天津</option>
+      </select>
+    </div>
+    <div>
+      <!-- (重要)
+      遇到复选框, v-model的变量值
+      非数组 - 关联的是复选框的checked属性
+      数组   - 关联的是复选框的value属性
+       -->
+      <span>爱好: </span>
+      <input type="checkbox" v-model="hobby" value="抽烟">抽烟
+      <input type="checkbox" v-model="hobby" value="喝酒">喝酒
+      <input type="checkbox" v-model="hobby" value="写代码">写代码
+    </div>
+    <div>
+      <span>性别: </span>
+      <input type="radio" value="男" name="sex" v-model="gender">男
+      <input type="radio" value="女" name="sex" v-model="gender">女
+    </div>
+    <div>
+      <span>自我介绍</span>
+      <textarea v-model="intro"></textarea>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
+  
   data() {
     return {
-      message: "HELLO, WORLD",
+      username: "",
+      pass: "",
+      from: "",
+      hobby: [], 
+      sex: "",
+      intro: "",
     };
-  },
-  methods: {
-    btn(){
-            // // 3. 截取字符串返回数组
-      // let arr = this.message.split("")
-      // // 4. 翻转
-      // arr.reverse()
-      // // 5. 数组拼接起来
-      // this.message = arr.join("")
-
-      // 简化
-      this.message = this.message.split("").reverse().join("")
-    }
+    // 总结:
+    // 特别注意: v-model, 在input[checkbox]的多选框状态
+    // 变量为非数组, 则绑定的是checked的属性(true/false) - 常用于: 单个绑定使用
+    // 变量为数组, 则绑定的是他们的value属性里的值 - 常用于: 收集勾选了哪些值
   }
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
