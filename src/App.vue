adb5bcb8da63068684a30591b5a05552edbc50c0
kotoriefreet <1063377232@qq.com>
2022-06-28 19:49:47 +0800

    v-bind

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,21 @@
 <template>
   <div id="app">
-    <h1>{{ msg }}</h1>
-    <h2>{{ obj.name }}</h2>
-    <h3>{{ obj.age > 18 ? '成年' : '未成年' }}</h3>
+ <a v-bind:href="url">点击去百度</a>
+    <!-- 语法: :原生属性名="vue变量" -->
+    <img :src="imgUrl" />
+    <img :src="localImg">
   </div>
 </template>
 
 <script>
-
+import imgObj from './assets/logo.png'
 export default {
   name: 'App',
   data() { // 格式固定, 定义vue数据之处
     return {  // key相当于变量名
-      msg: "hello, vue",
-      obj: {
-        name: "小vue",
-        age: 5
-      }
+      url: 'http://www.baidu.com',
+      imgUrl: "http://yun.itheima.com/Upload/./Images/20210412/60741c11ab77b.jpg",
+      localImg: imgObj
     }
   }
 }
